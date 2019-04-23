7d581b7e30adc9da8c50d2c7d8019cd580b25d11
aPeter1 <42580833+aPeter1@users.noreply.github.com>
2019-04-23 17:38:27 -0600

    Adding UI for reading in unexpected formats in the .dat files

--- beams_controller.py
+++ beams_controller.py
@@ -2,6 +2,8 @@
 
 import beams_view
 import beams_model
+
+import numpy as np
 from PyQt5 import QtWidgets, QtCore
 import sys
 import os
@@ -9,16 +11,16 @@ import os
 class BEAMSController():
     def __init__(self):
         super(BEAMSController,self).__init__()
-    
-    def start_program(self):
-        app = QtWidgets.QApplication(sys.argv)
+        self.app = QtWidgets.QApplication(sys.argv)
         self.BEAMS_view = beams_view.MainGUIWindow()
         self.BEAMS_database = beams_model.RunDataBase()
-        self.set_events()
+        self.set_main_events()
+    
+    def start_program(self):
         self.BEAMS_view.show()
-        sys.exit(app.exec_())
+        sys.exit(self.app.exec_())
 
-    def set_events(self):
+    def set_main_events(self):
         self.BEAMS_view.run_display.isolate_button.released.connect(lambda: self.isolate_click())
         self.BEAMS_view.run_display.inspect_button.released.connect(lambda: self.inspect_click())
         self.BEAMS_view.run_display.plot_all_button.released.connect(lambda: self.plot_all_click())
@@ -27,17 +29,28 @@ class BEAMSController():
         self.BEAMS_view.file_manager.write_button.released.connect(lambda: self.write_click())
         self.BEAMS_view.file_manager.plot_button.released.connect(lambda: self.plot_click())
 
-        self.BEAMS_view.plot_editor.input_xmax_one.returnPressed.connect(lambda: self.xvalue_change())
-        self.BEAMS_view.plot_editor.input_xmax_two.returnPressed.connect(lambda: self.xvalue_change())
-        self.BEAMS_view.plot_editor.input_xmin_one.returnPressed.connect(lambda: self.xvalue_change())
-        self.BEAMS_view.plot_editor.input_xmin_two.returnPressed.connect(lambda: self.xvalue_change())
-        self.BEAMS_view.plot_editor.input_slider_one.returnPressed.connect(lambda: self.bin_value_change())
-        self.BEAMS_view.plot_editor.input_slider_two.returnPressed.connect(lambda: self.bin_value_change())
-        self.BEAMS_view.plot_editor.slider_one.sliderMoved.connect(lambda: self.slider_moved())
-        self.BEAMS_view.plot_editor.slider_two.sliderMoved.connect(lambda: self.slider_moved())
+        self.BEAMS_view.plot_editor.input_xmax_one.returnPressed.connect(lambda: self.xvalue_change(plot=1,xtype="MAX",\
+            xvalue=float(self.BEAMS_view.plot_editor.input_xmax_one.text())))
+        self.BEAMS_view.plot_editor.input_xmax_two.returnPressed.connect(lambda: self.xvalue_change(plot=2,xtype="MAX",\
+            xvalue=float(self.BEAMS_view.plot_editor.input_xmax_two.text())))
+        self.BEAMS_view.plot_editor.input_xmin_one.returnPressed.connect(lambda: self.xvalue_change(plot=1,xtype="MIN",\
+            xvalue=float(self.BEAMS_view.plot_editor.input_xmin_one.text())))
+        self.BEAMS_view.plot_editor.input_xmin_two.returnPressed.connect(lambda: self.xvalue_change(plot=2,xtype="MIN",\
+            xvalue=float(self.BEAMS_view.plot_editor.input_xmin_two.text())))
+
+        self.BEAMS_view.plot_editor.input_slider_one.returnPressed.connect(lambda: self.bin_value_change(plot=1,\
+            bin_value=int(self.BEAMS_view.plot_editor.input_slider_one.text())))
+        self.BEAMS_view.plot_editor.input_slider_two.returnPressed.connect(lambda: self.bin_value_change(plot=2,\
+            bin_value=int(self.BEAMS_view.plot_editor.input_slider_two.text())))
+
+        self.BEAMS_view.plot_editor.slider_one.sliderMoved.connect(lambda: self.slider_moved(plot=1,moving=True))
+        self.BEAMS_view.plot_editor.slider_two.sliderMoved.connect(lambda: self.slider_moved(plot=2,moving=True))
+        self.BEAMS_view.plot_editor.slider_one.sliderReleased.connect(lambda: self.slider_moved(plot=1,moving=False))
+        self.BEAMS_view.plot_editor.slider_two.sliderReleased.connect(lambda: self.slider_moved(plot=2,moving=False))
 
         self.BEAMS_view.exit_act.triggered.connect(QtWidgets.qApp.quit)
         self.BEAMS_view.add_data_act.triggered.connect(lambda: self.import_click())
+        self.BEAMS_view.format_act.triggered.connect(lambda: self.launch_formatter())
 
     def isolate_click(self):
         return
@@ -58,20 +71,112 @@ class BEAMSController():
         print("Write button currently has no functionality ... ")
 
     def plot_click(self):
-        checked_items = set()
+        beams_checked_items = set()
         for index in range(self.BEAMS_view.file_manager.file_list.count()):
             if self.BEAMS_view.file_manager.file_list.item(index).checkState() == QtCore.Qt.Checked:
-                checked_items.add(self.BEAMS_view.file_manager.file_list.item(index).text())
-        self.BEAMS_database.read_all_files(checked_items)
+                beams_checked_items.add(self.BEAMS_view.file_manager.file_list.item(index).text())
+        non_beams_files = self.BEAMS_database.read_all_files(beams_checked_items)
+        if non_beams_files:
+            self.invalid_file_format(filenames=non_beams_files)
 
     def plot_all_click(self):
         return
 
-    def xvalue_change(self):
+    def xvalue_change(self,plot=None,xtype=None,xvalue=None):
         return
 
-    def bin_value_change(self):
+    def bin_value_change(self,plot=None,bin_value=None):
         return
 
-    def slider_moved(self):
+    def slider_moved(self,plot=None,moving=False):
         return
+
+    def invalid_file_format(self,filenames=None):
+        self.launch_formatter(filenames)
+
+    def launch_formatter(self,filenames=None):
+        self.file_formatter = beams_view.FileFormatterUI(filenames=filenames)
+        self.set_formatter_events()
+        self.file_formatter.file_display.setPlainText(open(filenames[0]).read())
+        self.file_formatter.show()
+
+    def set_formatter_events(self):
+        self.file_formatter.check_asym.stateChanged.connect(lambda: self.formatter_checkbox(dtype="asym"))
+        self.file_formatter.check_bhist.stateChanged.connect(lambda: self.formatter_checkbox(dtype="bhist"))
+        self.file_formatter.check_error.stateChanged.connect(lambda: self.formatter_checkbox(dtype="error"))
+        self.file_formatter.check_fhist.stateChanged.connect(lambda: self.formatter_checkbox(dtype="fhist"))
+        self.file_formatter.check_lhist.stateChanged.connect(lambda: self.formatter_checkbox(dtype="lhist"))
+        self.file_formatter.check_other.stateChanged.connect(lambda: self.formatter_checkbox(dtype="other"))
+        self.file_formatter.check_rhist.stateChanged.connect(lambda: self.formatter_checkbox(dtype="rhist"))
+        self.file_formatter.check_time.stateChanged.connect(lambda: self.formatter_checkbox(dtype="time"))
+        self.file_formatter.check_uncertain.stateChanged.connect(lambda: self.formatter_checkbox(dtype="uncertain"))
+
+        self.file_formatter.apply_button.released.connect(lambda: self.formatter_apply_click(apply_all=False))
+        self.file_formatter.apply_all_button.released.connect(lambda: self.formatter_apply_click(apply_all=True))
+        self.file_formatter.done_button.released.connect(lambda: self.formatter_done_click())
+        self.file_formatter.file_list.currentIndexChanged.connect(lambda: self.formatter_file_changed())
+
+    def formatter_checkbox(self,dtype=None):
+        if dtype == "asym":
+            self.file_formatter.spin_asym.setEnabled(False) if self.file_formatter.spin_asym.isEnabled() \
+                else self.file_formatter.spin_asym.setEnabled(True)
+        elif dtype == "bhist":
+            self.file_formatter.spin_bhist.setEnabled(False) if self.file_formatter.spin_bhist.isEnabled() \
+                else self.file_formatter.spin_bhist.setEnabled(True)
+        elif dtype == "error":
+            self.file_formatter.spin_error.setEnabled(False) if self.file_formatter.spin_error.isEnabled() \
+                else self.file_formatter.spin_error.setEnabled(True)
+        elif dtype == "fhist":
+            self.file_formatter.spin_fhist.setEnabled(False) if self.file_formatter.spin_fhist.isEnabled() \
+                else self.file_formatter.spin_fhist.setEnabled(True)
+        elif dtype == "lhist":
+            self.file_formatter.spin_lhist.setEnabled(False) if self.file_formatter.spin_lhist.isEnabled() \
+                else self.file_formatter.spin_lhist.setEnabled(True)
+        elif dtype == "other":
+            self.file_formatter.spin_other.setEnabled(False) if self.file_formatter.spin_other.isEnabled() \
+                else self.file_formatter.spin_other.setEnabled(True)
+        elif dtype == "rhist":
+            self.file_formatter.spin_rhist.setEnabled(False) if self.file_formatter.spin_rhist.isEnabled() \
+                else self.file_formatter.spin_rhist.setEnabled(True)
+        elif dtype == "time":
+            self.file_formatter.spin_time.setEnabled(False) if self.file_formatter.spin_time.isEnabled() \
+                else self.file_formatter.spin_time.setEnabled(True)
+        else:
+            self.file_formatter.spin_uncertain.setEnabled(False) if self.file_formatter.spin_uncertain.isEnabled() \
+                else self.file_formatter.spin_uncertain.setEnabled(True)
+
+    def formatter_apply_click(self,apply_all=False):
+        data_sections = dict()
+        if self.file_formatter.check_asym.isChecked():
+            data_sections['asym'] = self.file_formatter.spin_asym.value() 
+        if self.file_formatter.check_bhist.isChecked():
+            data_sections['bhist'] = self.file_formatter.spin_bhist.value()
+        if self.file_formatter.check_error.isChecked(): 
+            data_sections['error'] = self.file_formatter.spin_error.value()
+        if self.file_formatter.check_fhist.isChecked(): 
+            data_sections['fhist'] = self.file_formatter.spin_fhist.value() 
+        if self.file_formatter.check_lhist.isChecked():
+            data_sections['lhist'] = self.file_formatter.spin_lhist.value() 
+        if self.file_formatter.check_other.isChecked():
+            data_sections['other'] = self.file_formatter.spin_other.value() 
+        if self.file_formatter.check_rhist.isChecked():
+            data_sections['rhist'] = self.file_formatter.spin_rhist.value() 
+        if self.file_formatter.check_time.isChecked():
+            data_sections['time'] = self.file_formatter.spin_time.value() 
+        if self.file_formatter.check_uncertain.isChecked():
+            data_sections['uncertain'] = self.file_formatter.spin_uncertain.value() 
+
+        if not apply_all:
+            self.BEAMS_database.read_unrecognized_format(sections=data_sections,\
+                filename=self.file_formatter.file_list.currentText())
+
+    def formatter_done_click(self):
+        # FIXME Take data before closing
+        self.file_formatter.close()
+
+    def formatter_file_changed(self):
+        return
+
+
+
+
--- beams_model.py
+++ beams_model.py
@@ -1,8 +1,9 @@
 # Model for BEAMS application
 
 import os
-import pandas as pd #Going to do tests to see if numpy or pandas is faster.
+import pandas as pd 
 import numpy as np
+import time
 
 class RunDataBase():
     def __init__(self):
@@ -15,7 +16,8 @@ class RunDataBase():
         remove_files = self.current_read_files.difference(filenames)
         new_files = filenames.difference(self.current_read_files)
         self.remove_runs(remove_files)
-        self.add_runs(new_files)
+        corrupt_dat_files = self.add_runs(new_files)
+        return corrupt_dat_files
 
     def remove_runs(self,remove_files):
         for filename in remove_files:
@@ -26,33 +28,96 @@ class RunDataBase():
                     self.run_list.remove(data)
 
     def add_runs(self,new_files):
+        non_beams_files = []
         for filename in new_files:
             file_ext = os.path.splitext(os.path.basename(filename))[1]
             if(file_ext == ".dat"):
-                print("Reading",filename,"...")
-                self.current_read_files.add(filename)
-                self.run_list.append(RunData(filename=self.find_full_file(filename)))
+                full_file = self.find_full_file(filename)
+                if self.is_BEAMS(full_file):
+                    print("Reading",filename,"...")
+                    self.current_read_files.add(filename)
+                    self.run_list.append(RunData(filename=full_file))
+                else:
+                    non_beams_files.append(full_file)
             else:
                 print(filename,"::",file_ext," is unsupported file type")
+        return non_beams_files
 
     def find_full_file(self,file_root):
         for full_file in self.filenames:
             if(os.path.split(full_file)[1] == file_root):
                 return full_file
 
+    def is_BEAMS(self,filename):
+        with open(filename) as file:
+            first_line = file.readline()
+        if first_line.split(None, 1)[0] == "BEAMS":
+            return True
+        return False
+
+    def read_unrecognized_files(self,sections=None,filename=None):
+        print(sections)
+        print(filename)
+
+
 class RunData():
-    def __init__(self,filename=None):
+    def __init__(self,filename=None,header_rows=1,hist_file=True):
         super(RunData,self).__init__()
         self.asymmetry = np.array([])
         self.time = np.array([])
         self.uncertainty = np.array([])
+        self.header_data = {}
         self.filename = filename
 
-        self.read_dat_file(filename)
+        if filename:
+            self.read_dat_file(filename=self.filename)
 
     def read_dat_file(self,filename=None):
-        data = pd.read_csv(filename)
-        print(data)
+        print("   Retrieving header data ...")
+        self.retrieve_header_data()
+        print("   Retrieving histogram data ...")
+        self.retrieve_histogram_data()
+        print("   Calculating Uncertainty ...")
+        self.calculate_uncertainty()
+        print("   Calculating Background Radiation ...")
+        self.calculate_background_radiation()
+        print("   Calculating Asymmetry ...")
+        self.calculate_asymmetry()
+        print("   Clearing Memory")
+        del self.histogram_data
+        print("Sucessfully read",filename)
+     
+    def retrieve_header_data(self):
+        header = pd.read_csv(self.filename,nrows=1,skiprows=1)
+        for title in header.columns:
+            self.header_data[title] = header.iloc[0][title]
+    
+    def retrieve_histogram_data(self,hist=None):
+        index = 1
+        while(pd.read_csv(self.filename,nrows=1,skiprows=index).size > 4): # Determines what line histogram data starts.
+            index += 1
+        if not hist: # If no specific histogram is specified
+            self.histogram_data = pd.read_csv(self.filename,skiprows=index)
+            self.histogram_data.columns = ['Back','Front','Right','Left']
+        else:
+            self.histogram_data = pd.read_csv(self.filename,skiprows=index,usecols=hist)
+          
+    def calculate_uncertainty(self):
+        d_front = np.sqrt(self.histogram_data['Front'])
+        d_back = np.sqrt(self.histogram_data['Back'])
+        self.uncertainty = np.sqrt(np.power((2*self.histogram_data['Back']/np.power(self.histogram_data['Front'] \
+             + self.histogram_data['Back'],2)*d_front),2) + np.power((2*self.histogram_data['Front'] \
+                 /np.power(self.histogram_data['Front'] + self.histogram_data['Back'],2)*d_back),2))
+        del d_front
+        del d_back
 
-    def update_asymmetry(self):
+    def calculate_background_radiation(self):
+        self.bkg_back = np.mean(self.histogram_data.iloc[70:800,0].values)
+        self.bkg_front = np.mean(self.histogram_data.iloc[70:800,1].values)
+
+    def calculate_asymmetry(self):
+        self.asymmetry = ((self.histogram_data['Back'] - self.bkg_back) - (self.histogram_data['Front'] - self.bkg_front))\
+            /((self.histogram_data['Front'] - self.bkg_front) + (self.histogram_data['Back'] - self.bkg_back))
+
+    def bin_data(self):
         return
