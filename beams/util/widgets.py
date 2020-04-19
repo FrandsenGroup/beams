efa74ffca51ec2d50094e99ec6bc0eaeaa596a62
Alec Petersen <k.alecpetersen@gmail.com>
2020-04-18 18:48:25 -0600

    File for extra small widgets I had to make for styling

---
+++ beams/util/widgets.py
@@ -0,0 +1,17 @@
+
+from PyQt5 import QtWidgets
+
+
+class StyleOneButton(QtWidgets.QPushButton):
+    def __init__(self, *args):
+        super(StyleOneButton, self).__init__(*args)
+
+
+class StyleTwoButton(QtWidgets.QPushButton):
+    def __init__(self, *args):
+        super(StyleTwoButton, self).__init__(*args)
+
+
+class StyleThreeButton(QtWidgets.QPushButton):
+    def __init__(self, *args):
+        super(StyleThreeButton, self).__init__(*args)
