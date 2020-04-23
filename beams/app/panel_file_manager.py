6c51d00d54064e9ab8613a459134339240ffa6da
Alec Petersen <k.alecpetersen@gmail.com>
2020-04-22 19:39:07 -0600

    Implemented most functionality

--- beams/app/panel_file_manager.py
+++ beams/app/panel_file_manager.py
@@ -1,8 +1,13 @@
 
+import os
+
 from PyQt5 import QtWidgets, QtCore
 
-from util import widgets
-from app.model import RunService
+from util import widgets, files
+from app.model import FileContext, MuonDataContext
+from app.dialog_misc import AddFileDialog
+from app.dialog_musr_download import MusrDownloadDialog
+from app.beams import BEAMS
 
 
 # noinspection PyArgumentList
@@ -65,7 +70,9 @@ class FileManagerPanel(QtWidgets.QDockWidget):
         # Set DockWidget to be fully laid out widget.
         self.setWidget(self._full_widget)
         self.setFloating(False)
-        self._set_callbacks()
+
+        # Connect to presenter
+        self._presenter = FileManagerPanelPresenter(self)
 
     def get_select_all_state(self):
         """
@@ -78,12 +85,12 @@ class FileManagerPanel(QtWidgets.QDockWidget):
         Gets all items currently listed.
         :return files: an array of files as titled in the view
         """
-        files = []
+        file_items = []
 
         for i in range(self.file_list.count()):
-            files.append(self.file_list.item(i).text())
+            file_items.append(self.file_list.item(i).text())
 
-        return files
+        return file_items
 
     def get_checked_items(self):
         """
@@ -94,7 +101,7 @@ class FileManagerPanel(QtWidgets.QDockWidget):
 
         for i in range(self.file_list.count()):
             if self.file_list.item(i).checkState() == QtCore.Qt.Checked:
-                checked_files.append(self.file_list.item(i).text)
+                checked_files.append(self.file_list.item(i).text())
 
         return checked_files
 
@@ -128,6 +135,12 @@ class FileManagerPanel(QtWidgets.QDockWidget):
         file_item.setFlags(file_item.flags() | QtCore.Qt.ItemIsUserCheckable)
         file_item.setCheckState(QtCore.Qt.Unchecked)
 
+    def add_items(self, titles):
+        for title in titles:
+            file_item = QtWidgets.QListWidgetItem(title, self.file_list)
+            file_item.setFlags(file_item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            file_item.setCheckState(QtCore.Qt.Unchecked)
+
     def check_all_items(self):
         """
         Sets check state of all items to 'checked.'
@@ -135,40 +148,48 @@ class FileManagerPanel(QtWidgets.QDockWidget):
         for i in range(self.file_list.count()):
             self.file_list.item(i).setCheckState(QtCore.Qt.Checked)
 
-    def uncheck_all_item(self):
+    def uncheck_all_items(self):
         """
         Sets check state of all items to 'unchecked.'
         """
         for i in range(self.file_list.count()):
             self.file_list.item(i).setCheckState(QtCore.Qt.Unchecked)
 
+    def clear_items(self):
+        for i in range(self.file_list.count()-1, -1, -1):
+            self.file_list.takeItem(i)
+
 
 class FileManagerPanelPresenter:
     def __init__(self, view):
-        self.view = view
-        self.service = RunService()
-
-        self._title_to_file_path = {}
+        self._view = view
+        self._model = FileManagerPanelModel(self)
+        self._set_callbacks()
 
     def _set_callbacks(self):
         """
         Sets the callbacks presenter to handle relevant signals from the view.
         """
-        self.view.import_button.released.connect(lambda: self._add_file_clicked())
-        self.view.write_button.released.connect(lambda: self._write_file_clicked())
-        self.view.plot_button.released.connect(lambda: self._plot_file_clicked())
-        self.view.convert_button.released.connect(lambda: self._convert_file_clicked())
-        self.view.remove_button.released.connect(lambda: self._remove_file_clicked())
-        self.view.select_all.stateChanged.connect(lambda: self._select_all_checked())
+        self._view.import_button.released.connect(lambda: self._add_file_clicked())
+        self._view.write_button.released.connect(lambda: self._write_file_clicked())
+        self._view.plot_button.released.connect(lambda: self._plot_file_clicked())
+        self._view.convert_button.released.connect(lambda: self._convert_file_clicked())
+        self._view.remove_button.released.connect(lambda: self._remove_file_clicked())
+        self._view.select_all.stateChanged.connect(lambda: self._select_all_checked())
 
     def _add_file_clicked(self):
-        pass
+        code = AddFileDialog.launch()
+
+        if code == AddFileDialog.Codes.FILE_SYSTEM:
+            self._get_files_from_system()
+        else:
+            MusrDownloadDialog.launch()
 
     def _remove_file_clicked(self):
-        pass
+        self._model.remove_files(self._view.get_checked_items())
 
     def _convert_file_clicked(self):
-        pass
+        self._model.convert_files(self._view.get_checked_items())
 
     def _plot_file_clicked(self):
         pass
@@ -180,7 +201,94 @@ class FileManagerPanelPresenter:
         """
         Handles the user checking or unchecking the 'select all' box.
         """
-        if self.view.get_select_all_state():
-            self.view.check_all_items()
+        if self._view.get_select_all_state():
+            self._view.check_all_items()
         else:
-            self.view.uncheck_all_items()
+            self._view.uncheck_all_items()
+
+    def _get_files_from_system(self):
+        # noinspection PyArgumentList
+        filenames = QtWidgets.QFileDialog.getOpenFileNames(self._view, 'Add file',
+                                                           BEAMS.load_last_used_directory())[0]
+        if len(filenames) > 0:
+            path = os.path.split(filenames[0])
+            BEAMS.set_last_used_directory(path[0])
+
+        self._model.add_files(filenames)
+        self._view.clear_items()
+        self._view.add_items(self._model.get_file_titles())
+
+    def update(self):
+        self._view.clear_items()
+        self._view.add_items(self._model.get_file_titles())
+
+
+class FileManagerPanelModel:
+    def __init__(self, observer=None):
+        self._data_context = MuonDataContext()
+        self._file_context = FileContext()
+        self._file_context.subscribe(self)
+
+        self._title_to_file_path = {}
+        self._observer = observer
+
+    def remove_files(self, file_paths):
+        self._file_context.remove_files([self._title_to_file_path[title] for title in file_paths])
+
+        for title in file_paths:
+            self._title_to_file_path.pop(title)
+
+    def add_files(self, file_paths):
+        for file_path in file_paths:
+            file_reader = files.file(file_path)
+            if file_reader.SOURCE == files.Source.BEAMS:
+                title = file_reader.read_meta()[files.TITLE_KEY]
+                if title not in self._title_to_file_path.keys():
+                    self._title_to_file_path[title] = file_path
+            else:
+                title = os.path.split(file_path)[1]
+                if title not in self._title_to_file_path.keys():
+                    self._title_to_file_path[title] = file_path
+
+        self._file_context.add_files(file_paths, stop_signal=True)
+
+    def update_files(self, file_paths):
+        self._title_to_file_path = {}
+        self.add_files(file_paths)
+
+    def convert_files(self, file_paths):
+        titles = []
+        removed_titles = []
+
+        for file_path in file_paths:
+            file_reader = files.file(self._title_to_file_path[file_path])
+            if file_reader.DATA_FORMAT == files.Format.BINARY:
+                title = os.path.split(file_path)[1]
+                removed_titles.append(title)
+
+                out_file = os.path.splitext(self._title_to_file_path[title])[0] + '.dat'
+                file_reader = file_reader.convert(out_file)
+
+                title = file_reader.read_meta()[files.TITLE_KEY]
+                titles.append(title)
+                self._title_to_file_path[title] = file_reader.file_path
+
+        self._file_context.remove_files([self._title_to_file_path[title] for title in removed_titles], stop_signal=True)
+
+        for title in removed_titles:
+            self._title_to_file_path.pop(title)
+
+        self._file_context.add_files([self._title_to_file_path[title] for title in titles])
+
+    def get_files(self):
+        return sorted(list(self._file_context.get_files()))
+
+    def get_file_titles(self):
+        return sorted(list(self._title_to_file_path.keys()))
+
+    def update(self):
+        self.update_files(self._file_context.get_files())
+        self.notify()
+
+    def notify(self):
+        self._observer.update()
