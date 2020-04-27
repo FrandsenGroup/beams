60c43beabefd905a49c0aa3588e69b5cf4fa3b20
Alec Petersen <k.alecpetersen@gmail.com>
2020-04-27 17:08:19 -0600

    Added dialog for warnings and permissions

--- beams/app/dialog_misc.py
+++ beams/app/dialog_misc.py
@@ -55,3 +55,85 @@ class AddFileDialog(QtWidgets.QDialog):
     def launch():
         dialog = AddFileDialog()
         return dialog.exec()
+
+
+# noinspection PyArgumentList
+class WarningMessageDialog(QtWidgets.QDialog):
+    def __init__(self, args=None):
+        super(WarningMessageDialog, self).__init__()
+        if args:
+            if len(args) == 2:
+                pos_function = args[1]
+                error_message = args[0]
+            else:
+                pos_function = None
+                error_message = args[0]
+        else:
+            error_message = None
+            pos_function = None
+
+        self.setWindowTitle('Error')
+        message = QtWidgets.QLabel(error_message)
+        pos_button = widgets.StyleOneButton('Okay')
+        self.setMinimumWidth(300)
+        self.setMinimumHeight(80)
+        pos_button.setFixedWidth(80)
+
+        if pos_function:
+            pos_button.released.connect(lambda: pos_function())
+
+        pos_button.released.connect(lambda: self.close())
+
+        col = QtWidgets.QVBoxLayout()
+
+        col.addWidget(message)
+        col.addWidget(pos_button)
+        col.setAlignment(message, QtCore.Qt.AlignCenter)
+        col.setAlignment(pos_button, QtCore.Qt.AlignCenter)
+        self.setLayout(col)
+
+    @staticmethod
+    def launch(args=None):
+        dialog = WarningMessageDialog(args)
+        return dialog.exec()
+
+
+# noinspection PyArgumentList
+class PermissionsMessageDialog(QtWidgets.QDialog):
+    class Codes(enum.IntEnum):
+        OKAY = 0
+        CANCEL = 1
+
+    def __init__(self, args):
+        super(PermissionsMessageDialog, self).__init__()
+        self.setWindowTitle('Permission')
+        message = QtWidgets.QLabel(args[0])
+        self.pos_button = widgets.StyleOneButton('Okay')
+        self.neg_button = widgets.StyleTwoButton('Cancel')
+        self.setMinimumWidth(300)
+        self.setMinimumWidth(80)
+        self.pos_button.setFixedWidth(80)
+        self.neg_button.setFixedWidth(80)
+
+        self.pos_button.released.connect(lambda: self.done(PermissionsMessageDialog.Codes.OKAY))
+
+        self.neg_button.released.connect(lambda: self.done(PermissionsMessageDialog.Codes.CANCEL))
+
+        self.neg_button.released.connect(lambda: self.close())
+        self.pos_button.released.connect(lambda: self.close())
+
+        col = QtWidgets.QVBoxLayout()
+        col.addWidget(message)
+        col.setAlignment(message, QtCore.Qt.AlignCenter)
+        row = QtWidgets.QHBoxLayout()
+        row.addWidget(self.pos_button)
+        row.addWidget(self.neg_button)
+        row.setAlignment(self.pos_button, QtCore.Qt.AlignRight)
+        row.setAlignment(self.neg_button, QtCore.Qt.AlignLeft)
+        col.addLayout(row)
+        self.setLayout(col)
+
+    @staticmethod
+    def launch(args):
+        dialog = PermissionsMessageDialog(args)
+        return dialog.exec()
