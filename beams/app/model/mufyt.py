347b67315324f21c442286dc43bd0a5c8e54e64f
Alec Petersen <k.alecpetersen@gmail.com>
2020-06-28 12:18:49 -0600

    fixed annoyingly small bug

--- beams/app/model/mufyt.py
+++ beams/app/model/mufyt.py
@@ -19,7 +19,7 @@ SIMPLE_GAUSSIAN = "f(t) = exp(-1/2*(\u03C3*t)^2)"
 GAUSSIAN_KT = "f(t) = 1/3 + 2/3*(1 - (\u03C3*t)^2)*exp(-1/2*(\u03C3*t)^2)"
 LORENTZIAN_KT = "f(t) = 1/3 + 2/3*(1 - \u03BB*t)*exp(-\u03BB*t)"
 COMBINED_KT = "f(t) = 1/3 + 2/3*(1-\u03C3^2*t^2-\u03BB*t)*exp(-\u03C3^2*t^2/2-\u03BB*t)"
-STRETCHED_KT = "f(t) = 1/3 + 2/3(1-(\u03C3*t)^\u03B2)*exp(-(\u03C3*t)^\u03B2/\u03B2"
+STRETCHED_KT = "f(t) = 1/3 + 2/3*(1-(\u03C3*t)^\u03B2)*exp(-(\u03C3*t)^\u03B2/\u03B2)"
 COSINE = "f(t) = cos(2*\u03C0*v*t + \u03C0*\u03D5/180)"
 INTERNAL_COSINE = "f(t) = \u03B1*cos(2*\u03C0*v*t + \u03C0*\u03D5/180)*exp(-\u03BB*t) + (1-\u03B1)*exp(-\u03BB*t)"
 BESSEL = "f(t) = j0*(2*\u03C0*v*t + \u03C0*\u03D5/180)"
@@ -192,7 +192,7 @@ def lambdify(expression, variables: dict, independent_variable):
     var_names.extend([var for var in variables.keys()])
     var_guesses = [float(data[0]) for data in variables.values()]
 
-    lambda_expression = sp.lambdify(var_names, sp.sympify(expression), "numpy")
+    lambda_expression = sp.lambdify(var_names, sp.sympify(expression_string), "numpy")
 
     return var_guesses, None, lambda_expression
 
@@ -273,29 +273,3 @@ def fit_lm(expression, time, asymmetry, uncertainty, variables: dict, independen
         variables[k][0] = p
 
     return pars, cov, lambda_expression
-
-
-# def func(x, a, b, c):
-#     return a * np.exp(-b * x) + c
-
-# xdata = np.linspace(0, 4, 50)
-# y = func(xdata, 2.5, 1.3, 0.5)
-# np.random.seed(1729)
-# y_noise = 0.2 * np.random.normal(size=xdata.size)
-# ydata = y + y_noise
-# plt.plot(xdata, ydata, 'b-', label='data')
-# popt, pcov = curve_fit(func, xdata, ydata)
-#
-# plt.plot(xdata, func(xdata, *popt), 'r-',
-#     label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
-# popt, pcov = curve_fit(func, xdata, ydata, bounds=(0, [3., 1., 0.5]))
-#
-# plt.plot(xdata, func(xdata, *popt), 'g--',
-#     label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.legend()
-# plt.show()
-
-# ind, exp = split_expression(var_func)
-# print("Function = {}\nIndependent Variable = {}\nExpression = {}\nFree Constants = {}".format(var_func, ind, exp, parse(exp)))
