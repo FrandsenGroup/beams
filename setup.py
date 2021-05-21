e04916a5c0bf1836c2f6ab16797ad49487e173cc
Alec <apetersen@brevium.com>
2021-05-21 17:05:07 -0600

    I-37 Setup.py and requirements.txt updated

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name='beams',
     version='0.1.0',
     description='Basic and Effective Analysis for Muon-Spin Spectroscopy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alec Petersen, Ben Frandsen',
     author_email='k.alecpetersen@gmail.com',
     url='https://github.com/aPeter1/BEAMS',
-    packages=['beams'],
+    classifiers=[
+
+    ],
+    keywords="muon musr spin relaxation resonance spectroscopy",
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
@@ -16,5 +25,6 @@ setup(
         'matplotlib',
         'requests',
         'pyqt5'
-    ]
+    ],
+    python_requires="~=3.5"
 )
