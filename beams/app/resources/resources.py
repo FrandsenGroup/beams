b36fdfe25a4ab887b0e68162b6a2140368bcf5fb
Alec Petersen <k.alecpetersen@gmail.com>
2022-02-20 16:43:18 -0700

    I-66 Better method to dynamically locate resources

--- beams/app/resources/resources.py
+++ beams/app/resources/resources.py
@@ -4,61 +4,68 @@ Stores constants for the paths of all our non-python resource files.
 
 from pathlib import Path
 import os
+import sys
 
-DEVELOPMENT_PATH = str(Path('beams/app/resources'))
-PRODUCTION_PATH = str(Path('resources'))
 
-CURRENT_PATH = DEVELOPMENT_PATH if os.path.exists(DEVELOPMENT_PATH) else PRODUCTION_PATH
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath("./beams")
+
+    return os.path.join(base_path, relative_path)
 
 
 # Using Path from pathlib is an easy way to fix the slash direction issue when switch between windows and unix systems.
-LOGO_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/logo_3.jpg')))
-MAXIMIZE_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/maximize_black.png')))
-MINIMIZE_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/minimize_black.png')))
-RESTORE_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/restore_black.png')))
-CLOSE_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/close_black.png')))
-SPLASH_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/splash.jpg')))
-PLOTTING_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/plotting_icon.png')))
-HISTOGRAM_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/histo_icon.png')))
-FITTING_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/fitting_icon.png')))
-DOWNLOAD_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/download_icon.png')))
-QUESTION_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/question_icon.png')))
-PLOTTING_CLICKED_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/plotting_icon_clicked.png')))
-HISTOGRAM_CLICKED_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/histo_icon_clicked.png')))
-FITTING_CLICKED_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/fitting_icon_clicked.png')))
-DOWNLOAD_CLICKED_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/download_icon_clicked.png')))
-QUESTION_CLICKED_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/question_icon_clicked.png')))
-MENU_IMAGE = os.path.join(CURRENT_PATH, str(Path('icons/menu_icon.png')))
-LOADING_GIF = os.path.join(CURRENT_PATH, str(Path('icons/loading.gif')))
+LOGO_IMAGE = resource_path(str(Path('app/resources/icons/logo_3.jpg')))
+MAXIMIZE_IMAGE = resource_path(str(Path('app/resources/icons/maximize_black.png')))
+MINIMIZE_IMAGE = resource_path(str(Path('app/resources/icons/minimize_black.png')))
+RESTORE_IMAGE = resource_path(str(Path('app/resources/icons/restore_black.png')))
+CLOSE_IMAGE = resource_path(str(Path('app/resources/icons/close_black.png')))
+SPLASH_IMAGE = resource_path(str(Path('app/resources/icons/splash.jpg')))
+PLOTTING_IMAGE = resource_path(str(Path('app/resources/icons/plotting_icon.png')))
+HISTOGRAM_IMAGE = resource_path(str(Path('app/resources/icons/histo_icon.png')))
+FITTING_IMAGE = resource_path(str(Path('app/resources/icons/fitting_icon.png')))
+DOWNLOAD_IMAGE = resource_path(str(Path('app/resources/icons/download_icon.png')))
+QUESTION_IMAGE = resource_path(str(Path('app/resources/icons/question_icon.png')))
+PLOTTING_CLICKED_IMAGE = resource_path(str(Path('app/resources/icons/plotting_icon_clicked.png')))
+HISTOGRAM_CLICKED_IMAGE = resource_path(str(Path('app/resources/icons/histo_icon_clicked.png')))
+FITTING_CLICKED_IMAGE = resource_path(str(Path('app/resources/icons/fitting_icon_clicked.png')))
+DOWNLOAD_CLICKED_IMAGE = resource_path(str(Path('app/resources/icons/download_icon_clicked.png')))
+QUESTION_CLICKED_IMAGE = resource_path(str(Path('app/resources/icons/question_icon_clicked.png')))
+MENU_IMAGE = resource_path(str(Path('app/resources/icons/menu_icon.png')))
+LOADING_GIF = resource_path(str(Path('app/resources/icons/loading.gif')))
 
-LATO_BLACK_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Black.ttf')))
-LATO_BLACK_ITALIC_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-BlackItalic.ttf')))
-LATO_BOLD_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Bold.ttf')))
-LATO_BOLD_ITALIC_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-BoldItalic.ttf')))
-LATO_ITALIC_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Italic.ttf')))
-LATO_LIGHT_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Light.ttf')))
-LATO_LIGHT_ITALIC_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-LightItalic.ttf')))
-LATO_REGULAR_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Regular.ttf')))
-LATO_THIN_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-Thin.ttf')))
-LATO_THIN_ITALIC_FONT = os.path.join(CURRENT_PATH, str(Path('fonts/fonts-ThinItalic.ttf')))
+LATO_BLACK_FONT = resource_path(str(Path('app/resources/fonts/fonts-Black.ttf')))
+LATO_BLACK_ITALIC_FONT = resource_path(str(Path('app/resources/fonts/fonts-BlackItalic.ttf')))
+LATO_BOLD_FONT = resource_path(str(Path('app/resources/fonts/fonts-Bold.ttf')))
+LATO_BOLD_ITALIC_FONT = resource_path(str(Path('app/resources/fonts/fonts-BoldItalic.ttf')))
+LATO_ITALIC_FONT = resource_path(str(Path('app/resources/fonts/fonts-Italic.ttf')))
+LATO_LIGHT_FONT = resource_path(str(Path('app/resources/fonts/fonts-Light.ttf')))
+LATO_LIGHT_ITALIC_FONT = resource_path(str(Path('app/resources/fonts/fonts-LightItalic.ttf')))
+LATO_REGULAR_FONT = resource_path(str(Path('app/resources/fonts/fonts-Regular.ttf')))
+LATO_THIN_FONT = resource_path(str(Path('app/resources/fonts/fonts-Thin.ttf')))
+LATO_THIN_ITALIC_FONT = resource_path(str(Path('app/resources/fonts/fonts-ThinItalic.ttf')))
 
-PSI_LINUX_CONVERSION = os.path.join(CURRENT_PATH, str(Path('binaries/PSI_LINUX')))
-PSI_WINDOWS_CONVERSION = os.path.join(CURRENT_PATH, str(Path('binaries/PSI_WINDOWS.exe')))
-TRIUMF_LINUX_CONVERSION = os.path.join(CURRENT_PATH, str(Path('binaries/TRIUMF_LINUX')))
-TRIUMF_MAC_CONVERSION = os.path.join(CURRENT_PATH, str(Path('binaries/TRIUMF_MAC')))
-TRIUMF_WINDOWS_CONVERSION = os.path.join(CURRENT_PATH, str(Path('binaries/TRIUMF_WINDOWS.exe')))
+PSI_LINUX_CONVERSION = resource_path(str(Path('app/resources/binaries/PSI_LINUX')))
+PSI_WINDOWS_CONVERSION = resource_path(str(Path('app/resources/binaries/PSI_WINDOWS.exe')))
+TRIUMF_LINUX_CONVERSION = resource_path(str(Path('app/resources/binaries/TRIUMF_LINUX')))
+TRIUMF_MAC_CONVERSION = resource_path(str(Path('app/resources/binaries/TRIUMF_MAC')))
+TRIUMF_WINDOWS_CONVERSION = resource_path(str(Path('app/resources/binaries/TRIUMF_WINDOWS.exe')))
 
 DARK_COLOR = '#19232D'
 LIGHT_COLOR = '#FAFAFA'
 
 try:
-    CONFIGURATION_FILE = os.path.join(CURRENT_PATH, str(Path('app.config')))
+    CONFIGURATION_FILE = resource_path(str(Path('app/resources/app.config')))
 
     if not os.path.exists(CONFIGURATION_FILE):
         with open(CONFIGURATION_FILE, 'w+') as f:
             pass
 
-    LOG_FILE = os.path.join(CURRENT_PATH, str(Path('beams.log')))
+    LOG_FILE = resource_path(str(Path('app/resources/beams.log')))
 
     with open(LOG_FILE, 'w') as fp:
         fp.truncate(0)
