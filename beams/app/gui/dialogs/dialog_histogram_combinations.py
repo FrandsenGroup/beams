439b562e65a9b8b19664e8b6150bafa06a139c5d
Alec Petersen <k.alecpetersen@gmail.com>
2023-06-15 21:57:09 -0400

    I-257 Handle exact lists of histograms for large files

--- beams/app/gui/dialogs/dialog_histogram_combinations.py
+++ beams/app/gui/dialogs/dialog_histogram_combinations.py
@@ -18,7 +18,7 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
         super().__init__()
         self.__rows = {}  # id : row_number
 
-        self.setMaximumWidth(400)
+        self.setMinimumWidth(400)
 
         self.done_button = QtWidgets.QPushButton('Done')
         self.skip_button = QtWidgets.QPushButton('Don''t Combine')
@@ -37,9 +37,11 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
                                                    "The files you selected each have {} histograms. An example range "
                                                    "would be 1-32 and 33-64 for a file with 64 histograms if you wanted"
                                                    "two resulting histograms. Ranges can also wrap around (i.e. 48-16)."
+                                                   "Alternatively, you can specify EXACTLY which histograms to combine"
+                                                   "using the third column 'Exact #'s'."
                                                    .format(num_histograms))
 
-        self.dialog_description.setMaximumWidth(300)
+        # self.dialog_description.setMinimumWidth(300)
         self.dialog_description.setWordWrap(True)
 
         self._set_attributes()
@@ -51,13 +53,14 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
         self.cancel_button.released.connect(lambda: self.done(self.Codes.Cancel))
         self.skip_button.released.connect(lambda: self.done(self.Codes.Done))
 
-        self.combination_table.setColumnCount(4)
-        self.combination_table.setHorizontalHeaderLabels(["Start #", "End #", "Title", ""])
+        self.combination_table.setColumnCount(5)
+        self.combination_table.setHorizontalHeaderLabels(["Start #", "End #", "Exact #'s", "Title", ""])
         self.combination_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         self.combination_table.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         self.combination_table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
         self.combination_table.horizontalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
-        self.combination_table.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        self.combination_table.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+        self.combination_table.horizontalHeader().setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
 
         self.add_row()
 
@@ -80,6 +83,10 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
         item.setText("")
         self.combination_table.setItem(n, 2, item)
 
+        item = QtWidgets.QTableWidgetItem()
+        item.setText("")
+        self.combination_table.setItem(n, 3, item)
+
         remove_button = QtWidgets.QPushButton("-")
         remove_button.setFixedWidth(30)
         remove_button.released.connect(lambda: self.remove_row(row_id))
@@ -88,7 +95,7 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
         layout.setAlignment(qt_constants.AlignCenter)
         button_widget = QtWidgets.QWidget()
         button_widget.setLayout(layout)
-        self.combination_table.setCellWidget(n, 3, button_widget)
+        self.combination_table.setCellWidget(n, 4, button_widget)
 
     def get_empty_rows(self):
         num_empty = 0
@@ -96,7 +103,8 @@ class HistogramCombinationDialog(QtWidgets.QDialog):
         for i in range(self.combination_table.verticalHeader().count()):
             if self.combination_table.item(i, 0).text() == "" \
                     and self.combination_table.item(i, 1).text() == "" \
-                    and self.combination_table.item(i, 2).text() == "":
+                    and self.combination_table.item(i, 2).text() == "" \
+                    and self.combination_table.item(i, 3).text() == "":
                 num_empty += 1
 
         return num_empty
@@ -173,44 +181,65 @@ class HistogramCombinationDialogPresenter(QtCore.QObject):
         for i in range(num_rows):
             start_text = self._view.combination_table.item(i, 0).text()
             end_text = self._view.combination_table.item(i, 1).text()
-            title = self._view.combination_table.item(i, 2).text()
+            exact_text = self._view.combination_table.item(i, 2).text()
+            title = self._view.combination_table.item(i, 3).text()
 
             # Ignore the final row if it is empty.
-            if i == num_rows - 1 and start_text == '' and end_text == '' and title == '':
+            if i == num_rows - 1 and start_text == '' and end_text == '' and title == '' and exact_text == '':
                 continue
 
-            # Ensure start and end are both integers
-            try:
-                start = int(start_text) - 1
-            except ValueError:
-                WarningMessageDialog.launch(["Start value ('{}') in row {} is invalid.".format(start_text, i+1)])
-                return
+            if exact_text != '' and (start_text != '' or end_text != ''):
+                WarningMessageDialog.launch([f"You specified both a range of histograms and the exact list on row {i+1}"
+                                             f". Range will be ignored."])
 
-            try:
-                end = int(end_text)
-            except ValueError:
-                WarningMessageDialog.launch(["End value ('{}') in row {} is invalid.".format(end_text, i+1)])
-                return
+            if exact_text != '':
+                try:
+                    exact_histograms = [int(h.strip()) - 1 for h in exact_text.split(',')]
+                except ValueError:
+                    WarningMessageDialog.launch([f"The exact histograms specified in row {i+1} are invalid."])
+                    return
 
-            histogram_ranges.append((start, end, title))
+                histogram_ranges.append((True, exact_histograms, None, title))
+            else:
+                # Ensure start and end are both integers
+                try:
+                    start = int(start_text) - 1
+                except ValueError:
+                    WarningMessageDialog.launch(["Start value ('{}') in row {} is invalid.".format(start_text, i+1)])
+                    return
 
-        for start, end, title in histogram_ranges:
-            if start < 0 or end < 0 or end > self._num_histograms or start > self._num_histograms - 1:
-                # We are going to allow overlapping ranges, but start and ends need to make sense.
-                WarningMessageDialog.launch(["Histogram range of '{}' to '{}' is invalid.".format(start, end)])
-                return
+                try:
+                    end = int(end_text)
+                except ValueError:
+                    WarningMessageDialog.launch(["End value ('{}') in row {} is invalid.".format(end_text, i+1)])
+                    return
+
+                histogram_ranges.append((False, start, end, title))
+
+        for is_exact, start_or_exact, end, title in histogram_ranges:
+            if is_exact:
+                for i in start_or_exact:
+                    if i < 0 or i > self._num_histograms:
+                        WarningMessageDialog.launch([f"Histogram value of {i} is invalid"])
+                        return
+            else:
+                if start_or_exact < 0 or end < 0 or end > self._num_histograms or start_or_exact > self._num_histograms - 1:
+                    # We are going to allow overlapping ranges, but start and ends need to make sense.
+                    WarningMessageDialog.launch(["Histogram range of '{}' to '{}' is invalid.".format(start_or_exact, end)])
+                    return
 
             if title == '':
                 WarningMessageDialog.launch(["Please specify a title for every histogram."])
                 return
 
         for f in self._files:
-            starts = [r[0] for r in histogram_ranges]
-            ends = [r[1] for r in histogram_ranges]
-            names = [r[2] for r in histogram_ranges]
+            is_exacts = [r[0] for r in histogram_ranges]
+            starts_or_exacts = [r[1] for r in histogram_ranges]
+            ends = [r[2] for r in histogram_ranges]
+            names = [r[3] for r in histogram_ranges]
 
             try:
-                f.file.set_combine_format(starts, ends, names)
+                f.file.set_combine_format(is_exacts, starts_or_exacts, ends, names)
             except:
                 WarningMessageDialog.launch(["Invalid set of histogram ranges"])
                 return
